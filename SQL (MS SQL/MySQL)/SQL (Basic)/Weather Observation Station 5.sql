5c0e95e9b8678c2688abfc9970323c87ad83947e
Alain D. Godo Alonso <125923673+Alain-Godo@users.noreply.github.com>
2023-03-08 14:59:35 -0500

    Added Solutions

--- SQL (MS SQL/MySQL)/SQL (Basic)/Weather Observation Station 5.sql
+++ SQL (MS SQL/MySQL)/SQL (Basic)/Weather Observation Station 5.sql
@@ -1,3 +1,5 @@
+-- MS SQL Solution
+
 select * from (select top 1 city, LEN(city) cityLength from station order by cityLength ASC,city ASC) Minimum
 UNION
 select * from (select top 1 city, LEN(city) cityLength from station order by cityLength desc, city ASC) Maximum
