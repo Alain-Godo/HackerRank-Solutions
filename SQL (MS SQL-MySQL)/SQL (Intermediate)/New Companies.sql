0bc9de99553e0c62552ccfb64a0190a7d9acb720
Alain D. Godo Alonso <125923673+Alain-Godo@users.noreply.github.com>
2023-03-11 01:04:52 -0500

    Better Solution

--- SQL (MS SQL-MySQL)/SQL (Intermediate)/New Companies.sql
+++ SQL (MS SQL-MySQL)/SQL (Intermediate)/New Companies.sql
@@ -1,9 +1,12 @@
 -- MS SQL Solution
 
-select c.company_code,c.founder,
-(select count(distinct lead_manager_code) from lead_manager lm where lm.company_code = c.company_code),
-(select count(distinct senior_manager_code) from senior_manager sm where sm.company_code = c.company_code),
-(select count(distinct manager_code) from manager m where m.company_code = c.company_code),
-(select count(distinct employee_code) from employee e where e.company_code = c.company_code)
+select c.*,
+count(distinct e.lead_manager_code),
+count(distinct e.senior_manager_code),
+count(distinct e.manager_code),
+count(distinct e.employee_code)
 from company c
+inner join employee e
+    on c.company_code = e.company_code
+group by c.company_code, c.founder
 order by c.company_code
