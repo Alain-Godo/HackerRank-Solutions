3496a470e40fb4552799603c3137b12c8ccfb930
Alain D. Godo Alonso <125923673+Alain-Godo@users.noreply.github.com>
2023-03-11 18:52:37 -0500

    Added Solution

---
+++ SQL (MS SQL-MySQL)/SQL (Intermediate)/The Report.sql
@@ -0,0 +1,20 @@
+-- MS SQL Solution
+
+select case when g.grade >= 8 then s.name else NULL end n,
+g.grade, s.marks
+from students s
+inner join grades g
+    on (s.marks > g.min_mark) and (s.marks < g.max_mark)
+order by g.grade desc, n , s.marks
+
+-- Alternative
+
+-- with studentswithgrade as(
+--     select s.name name , s.marks marks,
+--     (select grade from grades where (s.marks >= min_mark) and (s.marks <=              max_mark)) grade
+--     from students s
+-- )
+-- select case when s.grade>=8 then s.name else NULL end n,
+-- s.grade, s.marks
+-- from studentswithgrade s
+-- order by s.grade desc, n , s.marks
