e85459ce1e050e474b312467a8360d19396d77bf
Alain D. Godo Alonso <125923673+Alain-Godo@users.noreply.github.com>
2023-03-11 18:54:50 -0500

    Added Solution

--- SQL (MS SQL-MySQL)/SQL (Intermediate)/The Report.sql
+++ SQL (MS SQL-MySQL)/SQL (Intermediate)/The Report.sql
@@ -11,7 +11,7 @@ order by g.grade desc, n , s.marks
 
 -- with studentswithgrade as(
 --     select s.name name , s.marks marks,
---     (select grade from grades where (s.marks >= min_mark) and (s.marks <=              max_mark)) grade
+--     (select grade from grades where (s.marks >= min_mark) and (s.marks <= max_mark)) grade
 --     from students s
 -- )
 -- select case when s.grade>=8 then s.name else NULL end n,
