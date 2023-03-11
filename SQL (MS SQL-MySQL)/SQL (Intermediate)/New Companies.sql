0a3a24ba3f875b03cf49756b77ae68f1a6c8da7a
Alain D. Godo Alonso <125923673+Alain-Godo@users.noreply.github.com>
2023-03-11 01:08:49 -0500

    Better Solution

--- SQL (MS SQL-MySQL)/SQL (Intermediate)/New Companies.sql
+++ SQL (MS SQL-MySQL)/SQL (Intermediate)/New Companies.sql
@@ -10,3 +10,14 @@ inner join employee e
     on c.company_code = e.company_code
 group by c.company_code, c.founder
 order by c.company_code
+
+-- Alternative
+--select c.*,
+--count(distinct e.lead_manager_code),
+--count(distinct e.senior_manager_code),
+--count(distinct e.manager_code),
+--count(distinct e.employee_code)
+--from company c, employee e
+--where c.company_code = e.company_code
+--group by c.company_code, c.founder
+--order by c.company_code
