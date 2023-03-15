2ce8e292cd62aa58656bd0db1e2952faee8cb65b
Alain D. Godo Alonso <125923673+Alain-Godo@users.noreply.github.com>
2023-03-14 21:05:20 -0400

    Added Solution

---
+++ SQL (MS SQL-MySQL)/SQL (Intermediate)/Challenges.sql
@@ -0,0 +1,27 @@
+create table #temp (hi int, na varchar(50), cnt int)
+
+insert into #temp
+-- MS SQL Solution
+
+select h.hacker_id hi, h.name na, count(h.name) cnt
+from Hackers h
+inner join Challenges c
+    on h.hacker_id = c.hacker_id
+group by h.hacker_id, h.name;
+
+
+with new_temp as(
+select t.hi hi, t.na na, t.cnt cnt, max(t.cnt) over () maxi,
+case
+    when (select count(*) from #temp tt where tt.cnt = t.cnt) = 1 then 'U'
+    else 'NU'
+end is_unique
+from #temp t
+)
+select nt.hi, nt.na, nt.cnt
+from new_temp nt
+where (nt.cnt = maxi) or (is_unique = 'U')
+order by nt.cnt desc, nt.hi
+
+
+
