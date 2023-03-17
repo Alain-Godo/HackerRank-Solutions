cc353e3ee0cd8a0f05ace482073271658c431620
Alain D. Godo Alonso <125923673+Alain-Godo@users.noreply.github.com>
2023-03-17 15:31:01 -0400

    Added Solution

---
+++ SQL (MS SQL-MySQL)/SQL (Basic)/Weather Observation Station 15.sql
@@ -0,0 +1,5 @@
+-- MySQL Solution
+
+select round(s1.long_w,4)
+from station s1
+where s1.lat_n = (select max(s2.lat_n) from station s2 where s2.lat_n < 137.2345 )
---
+++ SQL (MS SQL-MySQL)/SQL (Basic)/Weather Observation Station 16.sql
@@ -0,0 +1,5 @@
+-- MySQL Solution
+
+select round(min(lat_n),4)
+from station
+where lat_n > 38.7780
