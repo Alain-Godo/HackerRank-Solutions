ca783810b83162f96dd49cbab620fee93c29feb4
Piele Matei <99535423+mpiele@users.noreply.github.com>
2023-05-11 19:22:11 +0300

    Update Write a function.py

---
+++ Python/Built-ins/Write a function.py
@@ -0,0 +1,17 @@
+def is_leap(year):
+    leap = False
+    
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                leap = True
+            elif year % 400 != 0:
+                leap = False
+        elif year % 100 != 0:
+            leap = True
+    elif year % 4 != 0:
+        leap = False
+            
+    return leap
+
+year = int(input())
