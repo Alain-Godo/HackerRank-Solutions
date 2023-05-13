4b1f75642cbcdacfd04a9d652f73660c0bf2b983
Alain D. Godo Alonso <125923673+Alain-Godo@users.noreply.github.com>
2023-05-13 19:54:17 -0400

    Solution added

--- Python/Introduccion/Write a function.py
+++ Python/Introduccion/Write a function.py
@@ -12,24 +12,3 @@ def is_leap(year):
 
 year = int(input())
 print(is_leap(year))
-
-
-# Alternative solution (by mpiele)
-def is_leap(year):
-    leap = False
-
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                leap = True
-            elif year % 400 != 0:
-                leap = False
-        elif year % 100 != 0:
-            leap = True
-    elif year % 4 != 0:
-        leap = False
-
-    return leap
-
-year = int(input())
-print(is_leap(year))
