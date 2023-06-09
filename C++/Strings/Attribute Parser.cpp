6a171b6b8b42e5e186b0502cd352ec692017bf9b
Alain-Godo <alaindga@gmail.com>
2023-06-09 11:41:01 -0400

    Solution added

--- C++/Strings/Attribute Parser.cpp
+++ C++/Strings/Attribute Parser.cpp
@@ -11,20 +11,44 @@ using namespace std;
 int main() {
     int n, q;
     cin >> n >> q;
-    map<string,map<string,string> > tags;
-    for(int i=1;i<=n;i++){
-        string line;
+    vector<string> hrml;
+    vector<string> tags;
+    map<string,string> tags_value;
+    string line;
+    cin >> ws;
+    for(int i=0;i<n;i++){
         getline(cin,line);
-        if( i<=(n/2) ){
-            char aux; string tag_name;
+        line.erase(remove(line.begin(),line.end(),'\"'),line.end());
+        line.erase(remove(line.begin(),line.end(),'>'),line.end());
+
+        if(line.substr(0,2) == "</"){
+            tags.pop_back();
+        }
+        else{
+            string prefix_tag = "";
+            for(int j=0;j<tags.size();j++){
+                prefix_tag += tags[j] + ".";
+            }
             stringstream ss;
             ss << line;
-            ss >> aux;
-            ss >> tag_name;
-            while(ss.rdbuf()->in_avail()){
-                
+            char aux;
+            string tag,key,value;
+            ss >> aux >> tag;
+            tags.push_back(tag);
+            while(ss){
+                ss >> key >> aux >> value;
+                tags_value[prefix_tag + tag + "~" + key] = value;
             }
         }
-    }  
+    }
+    for(int i=0;i<q;i++){
+        getline(cin,line);
+        if(tags_value.find(line) != tags_value.end()){
+            cout << tags_value[line] << "\n";
+        }
+        else{
+            cout << "Not Found!\n";
+        }
+    }
     return 0;
 }
